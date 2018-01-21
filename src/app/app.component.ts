51c32da58e8e0626669067c7bbc63f1691d09a4f
Danielapariona <daniela.parionac@gmail.com>
2018-01-20 21:41:01 -0500

    Add list of stores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,12 @@ export class AppComponent {
   title = 'PlatziSquare';
   lat = -12.0862384;
   lng = -77.0226701;
+  lugares:any = [
+    {active: true, nombre: 'Floreria la Gardenia'},
+    {active: true, nombre: 'Donas la pasadita'},
+    {active: false, nombre: 'Veterinaria huellitas'},
+    {active: false, nombre: 'Sushi sushiroll'},
+    {active: true, nombre: 'Hotel de gracia'},
+    {active: true, nombre: 'Zapateria el Cavo'}
+  ]
 }
