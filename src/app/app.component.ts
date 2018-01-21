2e5a69ff16f8bb14c5f1ac00790127a333ab6d6a
Danielapariona <daniela.parionac@gmail.com>
2018-01-20 22:22:33 -0500

    Directive ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,11 @@ export class AppComponent {
   lat = -12.0862384;
   lng = -77.0226701;
   lugares:any = [
-    {active: true, nombre: 'Floreria la Gardenia'},
-    {active: true, nombre: 'Donas la pasadita'},
-    {active: false, nombre: 'Veterinaria huellitas'},
-    {active: false, nombre: 'Sushi sushiroll'},
-    {active: true, nombre: 'Hotel de gracia'},
-    {active: true, nombre: 'Zapateria el Cavo'}
+    {cercania: 1, distancia: 1, active: true, nombre: 'Floreria la Gardenia'},
+    {cercania: 1, distancia: 1.8, active: true, nombre: 'Donas la pasadita'},
+    {cercania: 2, distancia: 5, active: false, nombre: 'Veterinaria huellitas'},
+    {cercania: 3, distancia: 10, active: false, nombre: 'Sushi sushiroll'},
+    {cercania: 3, distancia: 35, active: true, nombre: 'Hotel de gracia'},
+    {cercania: 3, distancia: 120, active: true, nombre: 'Zapateria el Cavo'}
   ]
 }
