24ee526543ee350c9de36627b09e72c9a7e1d4f6
AlejandraCP <alejandracp710@gmail.com>
2018-01-24 19:42:46 -0500

    Agregando navegando entre componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AgmCoreModule } from '@agm/core';
-import { ResaltarDirective } from './directives/resaltar.directive'
-import { ContarClicksDirective } from './directives/contar-clicks.directive'
-
+import { ResaltarDirective } from './directives/resaltar.directive';
+import { ContarClicksDirective } from './directives/contar-clicks.directive';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {Routes, RouterModule} from '@angular/router';
+// import { Router } from '@angular/router/src/router';
+// import { RouterModule } from '@angular/router/src/router_module';
+import { DetalleComponent } from './detalle/detalle.component';
+import { LugaresComponent } from './lugares/lugares.component';
+const appRoutes: Routes = [
+  {path: '', component: LugaresComponent},
+  {path: 'lugares', component: LugaresComponent},
+  {path: 'detalle', component: DetalleComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
     ResaltarDirective,
-    ContarClicksDirective
+    ContarClicksDirective,
+    DetalleComponent,
+    LugaresComponent
   ],
   imports: [
     BrowserModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyBzjvPvtZmEJ05RgJ2ktI7l-pn4VRgmgDo'
-    })
+    }),
+    BrowserAnimationsModule,
+     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
